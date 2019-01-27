72f9bccc461c14ccbbf4346b9a5aade81e770b66
Andrew T <andrewtong0@gmail.com>
2019-01-27 00:32:10 -0800

    Interfacing with DataScraper.py

--- services/getProfHTML.py
+++ services/getProfHTML.py
@@ -1,7 +1,11 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from webscrap.DataScraper import parse_url
 
-def professorURL(firstName, lastName):
+
+def professorURL(name):
+    firstName = name.partition(' ')[0]
+    lastName = name.partition(' ')[2]
     url_page = 'https://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=University+of+British+Columbia&schoolID=1413&query=' + firstName + '+' + lastName
 
     html_page = urlopen(url_page)
@@ -13,8 +17,7 @@ def professorURL(firstName, lastName):
         link = (accessHTML.partition('<a href="')[2]).partition('">')[0]
 
         output = "https://www.ratemyprofessors.com" + link
-
-        return(output)
+        print(output)
+        parse_url(output)
     except:
         print("Invalid name or professor could not be found")
-        return ''
