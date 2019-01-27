8eb512bcc13992ecfd883e45cd2f35744cf58701
Brandon Chung <b.chung8612@gmail.com>
2019-01-26 17:50:27 -0800

    Added imports

--- services/database/db_client.py
+++ services/database/db_client.py
@@ -1,3 +1,7 @@
 """
 Boilerplate code for connecting to PostgreSQL 9.6 database.
 """
+from sqlalchemy import create_engine, not_
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
