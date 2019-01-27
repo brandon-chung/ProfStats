aabd706a3449579b02366b409b9dd500fc68bbad
Brandon Chung <b.chung8612@gmail.com>
2019-01-26 18:16:34 -0800

    Added boilerplate connection code and added todos

--- services/database/db_client.py
+++ services/database/db_client.py
@@ -1,7 +1,28 @@
 """
 Boilerplate code for connecting to PostgreSQL 9.6 database.
 """
-from sqlalchemy import create_engine, not_
-from sqlalchemy.engine.base import Engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
+from services.config import POSTGRESQL_URL
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker
+
+engine = create_engine(POSTGRESQL_URL)
+conn = engine.connect()
+
+
+def get_db_session():
+    """
+    Returns database connection session.
+    """
+    #TODO
+
+def init_db():
+    """
+    Initializes databases and tables.
+    """
+    #TODO
+
+def drop_db():
+    """
+    Drops the database (Clears it)
+    """
+    #TODO
