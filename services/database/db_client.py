90a690166d1286533c668494eb042c9db5284c7d
Brandon Chung <b.chung8612@gmail.com>
2019-01-26 21:23:20 -0800

    Update db_client.py

--- services/database/db_client.py
+++ services/database/db_client.py
@@ -3,7 +3,9 @@ Boilerplate code for connecting to PostgreSQL 9.6 database.
 """
 from services.config import POSTGRESQL_URL
 from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
+from sqlalchemy.orm import sessionmaker
+from services.database import models
+
 
 engine = create_engine(POSTGRESQL_URL)
 conn = engine.connect()
@@ -13,16 +15,19 @@ def get_db_session():
     """
     Returns database connection session.
     """
+    
     #TODO
 
 def init_db():
     """
     Initializes databases and tables.
     """
+    models.Base.metadata.create_all(engine)
     #TODO
 
 def drop_db():
     """
     Drops the database (Clears it)
     """
+    models.Base.metadata.drop_all(engine)
     #TODO
